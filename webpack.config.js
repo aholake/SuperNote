8afb55f80a1f051a941094f0d9f00dd67dffa08f
Loc Vo <v.locvt3@vinbrain.net>
2020-12-08 00:11:11 +0700

    Copy assets from source code

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -25,4 +26,15 @@ module.exports = {
             { test: /\.css$/, use: ['style-loader', 'css-loader'] }
         ]
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "src/manifest.json", to: "../manifest.json" },
+                { from: "src/popup.html", to: "../popup.html" },
+                { from: "src/popup.html", to: "../popup.html" },
+                { from: "src/assets/icons", to: "../icons" },
+                { from: "src/assets/_locales", to: "../_locales" },
+            ],
+        }),
+    ]
 };
